c8158a23d54a8a0116561d4df4d1168076e457a7
ajmilo16 <156456574+ajmilo16@users.noreply.github.com>
2024-04-22 21:51:56 -0400

    Add files via upload

---
+++ armband_all_updated/Inc/PulseSensor.h
@@ -0,0 +1,43 @@
+#ifndef PULSESENSOR_H
+#define PULSESENSOR_H
+
+#include "stdbool.h"
+#include "stdlib.h"
+#include "stm32l4xx_hal.h"
+#include "stm32l4xx_hal_adc.h"
+
+/* Macros */
+#define MAX_VOLT 3.2
+#define BUF_LENGTH 30
+#define SAMPLE_FREQ 20 		// 50 Hz sample frequency
+#define SAMPLE_PERIOD 50	// 20 ms sample frequency
+
+#define THRESH_DEFAULT MAX_VOLT / 2.0f
+
+/* Function Prototypes*/
+
+// init PulseSensor with adc it will use
+// probably don't need timer handle since that
+// will likely be handled in main, but maybe
+// calls read_adc() and get_pulse()
+void initPulseSensor(ADC_HandleTypeDef*);
+
+// Called when the timer overflows
+// can be used with polling or with 
+// timer callback
+void updatePulseSensor(void);
+
+// sets signal with converted value
+void read_ADC(void);
+
+// uses static signal and rate buffer
+// to update IBI and BPM
+void get_pulse(void);
+
+// simply returns value held in BPM var
+int get_BPM(void);
+
+// simply returns value held in IBI var
+int get_IBI(void);
+
+#endif
